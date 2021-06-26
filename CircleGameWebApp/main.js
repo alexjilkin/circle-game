2aa0dab98c08853bd904a77c32a30854584b8295
Alex Jilkin <jilkinalex@gmail.com>
2021-06-26 21:02:34 +0300

    Improve website

--- CircleGameWebApp/main.js
+++ CircleGameWebApp/main.js
@@ -1,10 +1,32 @@
-circleGameApiClient.getScores().then(scores => {
-    $scores = document.getElementsByClassName('scores')[0]
+(function main() {
+    let filterValue = '';
+    let scores = [];
+    let filteredScores = [];
 
-    scores.forEach(score => {
-        $score = document.createElement('div')
-        $score.innerHTML = `${score.name}: ${score.score}`
+    const  $scores = document.getElementsByClassName('scores')[0]
 
-        $scores.appendChild($score);
+    function renderScores(scores) {
+        $scores.innerHTML = ''
+        scores.forEach(score => {
+            $score = document.createElement('div')
+            $score.className = 'panel-block'
+            $score.innerHTML = `${score.name}: ${score.score}`
+            $scores.appendChild($score);
+        })
+    }
+
+    circleGameApiClient.getScores().then(res => {
+        scores = res
+        filteredScores = scores;
+        renderScores(scores)
     })
-})
+
+    const input = document.getElementById('input-filter')
+
+    input.addEventListener('input', e => {
+        filterValue = e.target.value
+        filteredScores = scores.filter(({name}) => name.toLowerCase().includes(filterValue.toLowerCase()))
+        renderScores(filteredScores)
+    });
+})()
+
